ea6d0548b6c4f85df7326f9a88597da411bff8ce
Gustavo Charamba <gucharamba@gmail.com>
2024-02-19 10:40:38 -0300

    UserResource get

---
+++ backend/src/main/java/cGustavo/backend/resources/UserResource.java
@@ -0,0 +1,31 @@
+package cGustavo.backend.resources;
+
+import cGustavo.backend.entities.User;
+import cGustavo.backend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+}
