41012e228c267424429f0fe12e454aa4861361cb
Gustavo Charamba <gucharamba@gmail.com>
2024-02-19 12:23:42 -0300

    httpGet users

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+import axios from "axios"
 
 const Home = () => {
+
+    const [users, setUsers] = useState([])
+
+    const getUsers = async () => {
+        try{
+            const response = await axios.get("http://localhost:8080/users")
+
+            console.log(response.data)
+       } catch (error){
+        console.log(error)
+       }
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
     return(
         <div>
             Home
