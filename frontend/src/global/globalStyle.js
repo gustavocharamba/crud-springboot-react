3cd655540788230fc65f9da9aaf39923b6073160
Gustavo Charamba <gucharamba@gmail.com>
2024-02-19 09:22:43 -0300

    dependencies intalled

---
+++ frontend/src/global/globalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        -webkit-font-smoothing: antialiased; 
+    }
+`
+
+export default GlobalStyle
