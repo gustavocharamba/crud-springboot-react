5408d4bbf17cb539ee50f7922447c8eed6dadd48
Gustavo Charamba <gucharamba@gmail.com>
2024-02-21 20:42:20 -0300

    userResource added post, delete and put

--- backend/src/main/java/cGustavo/backend/resources/UserResource.java
+++ backend/src/main/java/cGustavo/backend/resources/UserResource.java
@@ -5,7 +5,9 @@ import cGustavo.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -27,6 +29,28 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = userService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = userService.update(id, obj);
 
         return ResponseEntity.ok().body(obj);
     }
